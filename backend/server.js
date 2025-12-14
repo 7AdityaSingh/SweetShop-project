37eb3b027b7d10b6c2c7783c2058ad560657c677
7AdityaSingh <141657113+7AdityaSingh@users.noreply.github.com>
2025-12-14 22:27:41 +0530

    rename

--- backend/server.js
+++ backend/server.js
@@ -32,8 +32,8 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 if (require.main === module) {
-  app.listen(5000, () => {
-    console.log(`Server running on http://localhost:5000`);
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
